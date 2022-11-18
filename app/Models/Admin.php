db07d178e736d0a9a3868e63686f67fe653278f6
carnivuth <matti200042@gmail.com>
2022-11-18 16:22:46 +0100

    added file method

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,13 +3,48 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends \Illuminate\Foundation\Auth\User
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    public function permessions(){
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    public function permissions(){
         return $this->belongsToMany(Permission::class);
+    }
+    public function files(){
+        return $this->belongsToMany(File::class);
     }
 }
